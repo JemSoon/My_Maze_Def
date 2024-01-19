e9a87eb949669a99ab739eba45e81c91fa8ed141
JemSoon <97009299+JemSoon@users.noreply.github.com>
2024-01-20 07:49:53 +0900

    나중에 새로운 길이 추가되었을때 또 찾아가게끔 코드 추가

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -14,6 +14,7 @@ public class Monster : MonoBehaviour
     public CapsuleCollider2D monsterCollider;
     public ColliderType monsterColliderTyep;
     public bool isMoving = false;
+    public bool isArrived = false;
     float arrivalDistanceSquared; //목적지 도착 오차범위
 
 
@@ -38,7 +39,11 @@ public class Monster : MonoBehaviour
 
     void Update()
     {
-
+        //나중에 길이 오픈되었을때 한번 호출하게끔 바꾸자
+        if(isArrived && !isMoving) 
+        { 
+            FindWayPoint(waypointID); 
+        }
     }
 
     IEnumerator Moving()
@@ -56,20 +61,12 @@ public class Monster : MonoBehaviour
             //if (dir == Vector2.zero)
             if (dir.sqrMagnitude >= arrivalDistanceSquared) //오차 범위내에 도착했다면
             {
+                isArrived = true;
                 ++waypointID;
                 FindWayPoint(waypointID);
             }
 
             yield return null;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.layer == 0)
-        {
-            moveSpeed = 0;
-            isMoving = false;
         }
     }
 
@@ -84,8 +81,11 @@ public class Monster : MonoBehaviour
             if(point.pointNum == num)
             {
                 Debug.Log("다음 포인트 확인 고고고");
+                isMoving = true;
+                isArrived = false;
                 targetObject = point.gameObject;
                 isFound = true;
+                StartCoroutine(Moving());
                 break;
             }
         }
@@ -94,6 +94,15 @@ public class Monster : MonoBehaviour
         {
             isMoving = false;
             Debug.Log("길을 잃었다..목적지가 없다..");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 0)
+        {
+            moveSpeed = 0;
+            isMoving = false;
         }
     }
 }
