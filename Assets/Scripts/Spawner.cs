9746cc0aa41be592d8a2625fb0afd2b400dfcd97
JemSoon <97009299+JemSoon@users.noreply.github.com>
2024-02-01 10:24:31 +0900

    원하는 순서대로 몬스터 스폰 성공

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,14 +9,17 @@ public class Spawner : MonoBehaviour
     public int spawnCount = 0;//총 소환할 마리수
     int beginSpawnCount; //초기 설정 소환 마리수
     int nowCount = 0; //현재 소환한 마리수
-    int level;
+    int level; //레벨에 따른 몬스터 종류
     float timer;
     [Header("몇초마다 단계를 올립니까?")]
     public float seconds;
     //public Transform[] spanwPoint;
 
+    [Header("당신이 원하는 소환 순서")]
+    public MonsterType[] monsterTypes;
     private void Awake()
     {
+        spawnCount = monsterTypes.Length;
         //기본 소환 마리수 저장해두기
         beginSpawnCount = spawnCount;
     }
@@ -27,9 +30,9 @@ public class Spawner : MonoBehaviour
         timer += Time.deltaTime;
 
         //SpawnData의 배열을 넘지않게 제한
-        level = Mathf.Min(Mathf.FloorToInt(GameManager.Inst.gameTime / seconds),spawnData.Length-1);
+        //level = Mathf.Min(Mathf.FloorToInt(GameManager.Inst.gameTime / seconds),spawnData.Length-1);
 
-        if(timer > spawnData[level].spawnTime && nowCount<spawnCount)
+        if (timer > spawnData[(int)monsterTypes[level]].spawnTime && nowCount<spawnCount)
         {
             timer = 0;
             Spawn();
@@ -47,7 +50,14 @@ public class Spawner : MonoBehaviour
     {
         GameObject monster = GameManager.Inst.poolManager.Get(0);
         //monster.transform.position = spawnPoint[]
-        monster.GetComponent<Monster>().Init(spawnData[level]);
+
+        monster.GetComponent<Monster>().Init(spawnData[(int)monsterTypes[level]]);
+        
+        if(level<monsterTypes.Length-1)
+        {
+            ++level;
+        }
+
         ++nowCount;
     }
 
@@ -68,4 +78,12 @@ public class SpawnData
     public float speed;
 
     public int giveKeyCount;
+}
+
+[System.Serializable]
+public enum MonsterType
+{
+    red,
+    purple,
+    blue,
 }
