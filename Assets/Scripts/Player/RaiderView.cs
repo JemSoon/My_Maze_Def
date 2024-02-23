0359f449cf5e8d2abfceffc2549244d1d49eff90
JemSoon <97009299+JemSoon@users.noreply.github.com>
2024-02-23 19:29:05 +0900

    레이더를 만들었어용

--- Assets/Scripts/Player/RaiderView.cs
+++ Assets/Scripts/Player/RaiderView.cs
@@ -6,6 +6,7 @@ public class RaiderView : MonoBehaviour
 {
     Transform raiderTransform;
     Scanner scanner;
+    public SpriteRenderer raider2;
 
     // Start is called before the first frame update
     void Start()
@@ -16,4 +17,8 @@ public class RaiderView : MonoBehaviour
         raiderTransform.localScale = new Vector3(scanner.scanRange * 2, scanner.scanRange * 2, 1);
     }
 
+    private void FixedUpdate()
+    {
+        raider2.transform.Rotate(0, 0, Time.deltaTime * 100);
+    }
 }
