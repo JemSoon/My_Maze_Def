e4c72d5f0132408aa2f966f2038dcccf74f67696
JemSoon <97009299+JemSoon@users.noreply.github.com>
2024-07-01 02:48:34 +0900

    페북 SDK제거 + Assets/Plugins/Android/mainTemplate.gradle파일에 워닝 제거용 문구 추가
    dependencies {
        // 다른 종속성들
        implementation 'androidx.fragment:fragment:latest_version'
    }

--- Assets/Scripts/AD/FB_AD.cs
+++ Assets/Scripts/AD/FB_AD.cs
@@ -1,53 +1,53 @@
 using UnityEngine;
-using Facebook.Unity;
+//using Facebook.Unity;
 
 public class FB_AD : MonoBehaviour
 {
-    private bool IsInitFacebook = false; // 초기값은 초기화되지 않았음(false)으로 설정
+    //private bool IsInitFacebook = false; // 초기값은 초기화되지 않았음(false)으로 설정
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //facebook
-        if (!FB.IsInitialized)
-        {
-            // Initialize the Facebook SDK
-            FB.Init(InitCallback, OnHideUnity);
-        }
-        else
-        {
-            // Already initialized, signal an app activation App Event
-            FB.ActivateApp();
-        }
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    //facebook
+    //    if (!FB.IsInitialized)
+    //    {
+    //        // Initialize the Facebook SDK
+    //        FB.Init(InitCallback, OnHideUnity);
+    //    }
+    //    else
+    //    {
+    //        // Already initialized, signal an app activation App Event
+    //        FB.ActivateApp();
+    //    }
+    //}
 
-    private void InitCallback()
-    {
-        if (FB.IsInitialized)
-        {
-            // Signal an app activation App Event
-            FB.ActivateApp();
-            IsInitFacebook = true;
-            // Continue with Facebook SDK
-            // ...
-        }
-        else
-        {
-            Debug.Log("Failed to Initialize the Facebook SDK");
-        }
-    }
+    //private void InitCallback()
+    //{
+    //    if (FB.IsInitialized)
+    //    {
+    //        // Signal an app activation App Event
+    //        FB.ActivateApp();
+    //        IsInitFacebook = true;
+    //        // Continue with Facebook SDK
+    //        // ...
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Failed to Initialize the Facebook SDK");
+    //    }
+    //}
 
-    private void OnHideUnity(bool isGameShown)
-    {
-        if (!isGameShown)
-        {
-            // Pause the game - we will need to hide
-            Time.timeScale = 0;
-        }
-        else
-        {
-            // Resume the game - we're getting focus again
-            Time.timeScale = 1;
-        }
-    }
+    //private void OnHideUnity(bool isGameShown)
+    //{
+    //    if (!isGameShown)
+    //    {
+    //        // Pause the game - we will need to hide
+    //        Time.timeScale = 0;
+    //    }
+    //    else
+    //    {
+    //        // Resume the game - we're getting focus again
+    //        Time.timeScale = 1;
+    //    }
+    //}
 }
