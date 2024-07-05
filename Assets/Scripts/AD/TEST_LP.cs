a54f335ed72748036fc5de3f282a9447bd9c1ed1
JemSoon <97009299+JemSoon@users.noreply.github.com>
2024-07-05 20:30:00 +0900

    레벨플레이 SDK제거(...)후 EDM재설치, 애드몹 삽입전 백업 빌드 정상 작동 확인

--- Assets/Scripts/AD/TEST_LP.cs
+++ Assets/Scripts/AD/TEST_LP.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class TEST_LP : MonoBehaviour
 {
-    private static TEST_LP inst;
+    //private static TEST_LP inst;
 
-    public static TEST_LP Inst => inst;
+    //public static TEST_LP Inst => inst;
 
-    public static string APP_KEY => GetAppKey();
+    //public static string APP_KEY => GetAppKey();
 
-    private void Awake()
-    {
-        if(inst != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
+    //private void Awake()
+    //{
+    //    if(inst != null)
+    //    {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
 
-        inst = this;
-        DontDestroyOnLoad(gameObject);
+    //    inst = this;
+    //    DontDestroyOnLoad(gameObject);
 
-        InitializeAd();
+    //    InitializeAd();
 
-    }
+    //}
 
-    private static string GetAppKey()
-    {
-        return Application.platform switch
-        {
-            RuntimePlatform.Android => "your_android_app_key",
-            RuntimePlatform.IPhonePlayer => "your_iphone_app_key",
-            // î¤ Unity Editor ãÛÜ¬éÄ
-            RuntimePlatform.OSXEditor => "your_app_key",
-            _ => "unsupported_platform",
-        };
-    }
+    //private static string GetAppKey()
+    //{
+    //    return Application.platform switch
+    //    {
+    //        RuntimePlatform.Android => "your_android_app_key",
+    //        RuntimePlatform.IPhonePlayer => "your_iphone_app_key",
+    //        // î¤ Unity Editor ãÛÜ¬éÄ
+    //        RuntimePlatform.OSXEditor => "your_app_key",
+    //        _ => "unsupported_platform",
+    //    };
+    //}
 
-    private void InitializeAd()
-    {
-        IronSource.Agent.validateIntegration();
-        IronSource.Agent.init(APP_KEY);
-        IronSource.Agent.shouldTrackNetworkState(true);
-    }
+    //private void InitializeAd()
+    //{
+    //    IronSource.Agent.validateIntegration();
+    //    IronSource.Agent.init(APP_KEY);
+    //    IronSource.Agent.shouldTrackNetworkState(true);
+    //}
 }
